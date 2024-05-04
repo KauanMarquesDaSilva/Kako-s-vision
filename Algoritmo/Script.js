410665b57875f82f3935920caa01b1b310aa00cc
KauanMarquesDaSilva <kauan.silva@sptech.school>
2024-05-03 22:38:52 -0300

    Subindo atualizações Site

--- Algoritmo/Script.js
+++ Algoritmo/Script.js
@@ -1,14 +1,26 @@
-let time = 2000,
+let time = 10000,
     currentImageIndex = 0,
     images = document.querySelectorAll("#slider img")
     max = images.length;
 
+
 function NextImage (){
-    console.log(images)
+
+    images[currentImageIndex].classList.remove('selected')
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+    currentImageIndex = 0
+
+    images[currentImageIndex].classList.add('selected')
 }
 
+
 function start (){
-    
+    setInterval(() => {
+        NextImage()
+    }, time)
 }
 
 window.addEventListener("load", start)
