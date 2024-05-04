dfc363ddfab3e9da596d073e8edca2cea0579171
KauanMarquesDaSilva <kauan.silva@sptech.school>
2024-05-03 21:34:34 -0300

    Subdindo atualizações

---
+++ Algoritmo/Script.js
@@ -0,0 +1,14 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img")
+    max = images.length;
+
+function NextImage (){
+    console.log(images)
+}
+
+function start (){
+    
+}
+
+window.addEventListener("load", start)
